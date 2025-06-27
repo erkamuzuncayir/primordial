b1847135abdbb3b294eb4eeb2fa8bd37f2093f09
ErkamUzuncayir <erkamuzuncayir@gmail.com>
2025-06-27 17:46:17 +0300

    create: create math.cpp

---
+++ core/math/math.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ *  @file       math.cpp
+ *  @project    Primordial Game Engine
+ *  @module     Math Library
+ *  @brief      Core mathematical utilities and structures for 3D rendering,
+ *              physics, and engine-level numeric operations.
+ *
+ *  @author     H. Erkam Uzuncayir
+ *  @date       2025-06-27
+ *  @version    0.0.1
+ *
+ *  @license    This file is part of the Primordial Game Engine.
+ *              Primordial is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ *              
+ *              Primordial is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *              GNU General Public License for more details.
+ *
+ *              You should have received a copy of the GNU General Public License
+ *              along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  @note       This module is platform-independent and does not depend on any
+ *              external libraries beyond the C++ standard library.
+ ******************************************************************************/
+
